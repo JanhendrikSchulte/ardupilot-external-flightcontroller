7572dc9d001242995990486e4820450c27e38795
jan-hendrik.schulte <jan-hendrik.schulte@adesso.de>
2025-02-24 16:58:23 +0100

    mod stuff

--- build-ardupilot.sh
+++ build-ardupilot.sh
@@ -1,6 +1,6 @@
 #!/opt/homebrew/bin/fish
 cd $HOME/study/master/ardupilot-external-flightcontroller
 ./waf distclean
-./waf configure --board ext
+./waf configure --board sitl
 ./waf copter
 ./Tools/autotest/sim_vehicle.py -N -v ArduCopter -f gazebo-iris --model JSON --map --console
